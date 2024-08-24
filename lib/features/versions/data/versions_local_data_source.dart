ae2fc66885397fe79e75a580a7e2405946d3fce1
Resam Taghipour <resam.t@gmail.com>
2024-08-24 15:28:34 +0330

    Merge new Base for All

--- lib/features/versions/data/versions_local_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../core/failures/local_exception.dart';
-import '../../../data/storage/app_local_storage.dart';
-import '../models/app_version/app_version.dart';
-
-class VersionsLocalDataSource {
-  Future<Either<LocalException, AppVersionsList>> getVersions() async => AppLocalStorage.to.loadAppVersions();
-}
